b45593265a65947ec59718e84a4f7acb6627bd29
Matyas.Horky <horkyskolni@gmail.com>
2023-11-13 18:03:07 +0100

    Added file logging

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 import logging
 import time
 import shutil
-from difflib import Differ
 import filecmp
+from difflib import Differ
+from datetime import datetime
 # from tkinker.filedialog import askdirectory
 
 import constants
@@ -34,7 +35,7 @@ def askLocation(instruction_string):
     return path
 
 
-def syncFolders(differ):
+def syncFolders(differ, log_path):
     src_files = [constants.LOG_FILE]
     for src_p, src_f in absoluteFilePaths(source_path):
         src_files.append(src_f)
@@ -43,32 +44,50 @@ def syncFolders(differ):
         if os.path.exists(rep_p):
             # file exists in replica, check if it is updated
             if not filecmp.cmp(src_p, rep_p):
-                # if not rewrite it
-                if rep_p.endswith(tuple(constants.SUPPORTED_TYPES)):
-                    with open(src_p) as f1, open(rep_p) as f2:
-                        for line in differ.compare(f1.readlines(), f2.readlines()):
-                            print(line)
+                with open(log_path, 'a') as f:
+                    f.write(str(datetime.now()) + " ->\tUpdated file: " + rep_p + "\n")
+                    # if not rewrite it
+                    if rep_p.endswith(tuple(constants.SUPPORTED_TYPES)):
+                        with open(src_p) as f1, open(rep_p) as f2:
+                            for line in differ.compare(f2.readlines(), f1.readlines()):
+                                line.replace("\n", "")
+                                if line.startswith("+"):
+                                    f.write("\n")
+                                f.write("\t:" + line)
                 logging.info("File updated: " + rep_p)
                 shutil.copy(src_p, rep_p)
         else:
             # if the file doesn't exist in replica then create it
             logging.info("Added file: " + rep_p)
+            with open(log_path, 'a') as f:
+                f.write(str(datetime.now()) + " ->\tAdded file: " + rep_p + "\n")
             os.makedirs(os.path.dirname(rep_p), exist_ok=True)
             shutil.copy(src_p, rep_p)
+    # remove aditional files
     for rep_p, rep_f in absoluteFilePaths(replica_path):
         if not rep_f in src_files:
             logging.info("Removed file: " + rep_p)
+            with open(log_path, 'a') as f:
+                f.write(str(datetime.now()) + " ->\tRemoved file: " + rep_p + "\n")
             os.remove(rep_p)
         else:
             src_files.remove(rep_f)
+    # remove empty folders
+    deleteEmptyDirectory(replica_path)
 
 
 def absoluteFilePaths(directory):
     for dirpath, dirnames, filenames in os.walk(directory):
         for f in filenames:
             yield os.path.abspath(os.path.join(dirpath, f)), f
-        # for d in dirnames:
-        #     yield os.path.abspath(os.path.join(dirpath, d)), d
+
+
+def deleteEmptyDirectory(root_path):
+    for dirpath, dirnames, filenames in os.walk(root_path, topdown=False):
+        for d in dirnames:
+            path = os.path.join(dirpath, d)
+            if not os.listdir(path):
+                os.rmdir(path)
 
 
 if __name__ == '__main__':
@@ -79,6 +98,7 @@ if __name__ == '__main__':
     # for debug
     source_path = r"C:\Users\Horky\Desktop\source"
     replica_path = r"C:\Users\Horky\Desktop\replica"
+    log_path = replica_path + "\\" + constants.LOG_FILE
     if source_path == replica_path:
         # identical paths of source and replica
         print("Source path and target replica path must be different, terminating.")
@@ -93,7 +113,6 @@ if __name__ == '__main__':
         sys.exit()
     else:
         # all ok
-        # TODO log changes to file and logger
         differ = Differ()
         if len(os.listdir(replica_path)) > 0:
             print("Selected replica folder is not empty, all files will be deleted. Do you wish to continue? (Y/N)")
@@ -108,14 +127,17 @@ if __name__ == '__main__':
                 logging.info("Run aborted.")
                 time.sleep(constants.TERMINATION_DELAY)
                 sys.exit()
-        with open(replica_path + "\\" + constants.LOG_FILE, 'w') as f:
-            f.write(r"This file contains logs for changes at: " + source_path + "\n\n")
+        with open(log_path, 'w', encoding="UTF-8") as f:
+            f.write(
+                "This file contains logs for changes at: " + source_path + "\n\nSynchronization started at: " + str(
+                    datetime.now()) + "\n")
         print("Synchronization started.")
         logging.info("Synchro started with src: " + source_path + " and rep: " + replica_path)
         while True:
             for src_p, src_f in absoluteFilePaths(source_path):
                 if constants.LOG_FILE in src_f:
-                    logging.error("Source folder can't contain file named '" + constants.LOG_FILE + "'. Do you wish to remove it? (Y/N)")
+                    logging.error(
+                        "Source folder can't contain file named '" + constants.LOG_FILE + "'. Do you wish to remove it? (Y/N)")
                     if input() == "Y":
                         os.remove(src_p)
                     else:
@@ -126,4 +148,4 @@ if __name__ == '__main__':
             time.sleep(constants.SYNC_PERIOD)
             print("Updating")
             logging.info("Updating repository at: " + replica_path)
-            syncFolders(differ)
+            syncFolders(differ, log_path)
