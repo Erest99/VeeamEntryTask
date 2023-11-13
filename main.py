d052d659f9684b753581b4e36ad9511e3f3b4a36
Matyas.Horky <horkyskolni@gmail.com>
2023-11-13 20:10:12 +0100

    completed v1.0

--- main.py
+++ main.py
@@ -4,35 +4,12 @@ import logging
 import time
 import shutil
 import filecmp
+import tkinter
 from difflib import Differ
 from datetime import datetime
-# from tkinker.filedialog import askdirectory
+from tkinter import Tk, filedialog
 
 import constants
-
-
-def askLocation(instruction_string):
-    print(instruction_string)
-    attempt = 0
-    while True:
-        path = input()
-        if os.path.exists(path):
-            # valid path
-            break
-        else:
-            attempt += 1
-            if attempt >= constants.MAX_ATTEMPTS:
-                # exceeded max number of attempts
-                print("Exceeded maximum attempts, terminating")
-                logging.info("Exceeded maximum attempts(" + str(constants.MAX_ATTEMPTS) + "), terminating")
-                time.sleep(constants.TERMINATION_DELAY)
-                sys.exit()
-            else:
-                # not exceeded max number of attempts
-                print("Please enter valid path")
-                logging.info("wrong attempt num " + str(attempt))
-
-    return path
 
 
 def syncFolders(differ, log_path):
@@ -49,11 +26,13 @@ def syncFolders(differ, log_path):
                     # if not rewrite it
                     if rep_p.endswith(tuple(constants.SUPPORTED_TYPES)):
                         with open(src_p) as f1, open(rep_p) as f2:
+                            line_num = 1
+                            # minor formating bug
                             for line in differ.compare(f2.readlines(), f1.readlines()):
-                                line.replace("\n", "")
-                                if line.startswith("+"):
-                                    f.write("\n")
-                                f.write("\t:" + line)
+                                if line.startswith("+ ") or line.startswith("- "):  # not ideal
+                                    f.write("\t[" + str(line_num) + "]:" + line)
+                                    line_num += 1
+                            f.write("\n")
                 logging.info("File updated: " + rep_p)
                 shutil.copy(src_p, rep_p)
         else:
@@ -92,60 +71,62 @@ def deleteEmptyDirectory(root_path):
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    # TODO create option to browse folders
-    # source_path = askLocation("Enter source folder path.")
-    # replica_path = askLocation("Enter path to replica folder.")
     # for debug
-    source_path = r"C:\Users\Horky\Desktop\source"
-    replica_path = r"C:\Users\Horky\Desktop\replica"
+    root = tkinter.Tk()
+    root.withdraw()
+    print("Select source folder.")
+    source_path = filedialog.askdirectory().replace("/", "\\")
+    logging.info("Selected " + source_path + " as source")
+    print("Select folder to replicate to.")
+    replica_path = filedialog.askdirectory().replace("/", "\\")
+    logging.info("Selected " + replica_path + " as replica")
     log_path = replica_path + "\\" + constants.LOG_FILE
-    if source_path == replica_path:
-        # identical paths of source and replica
-        print("Source path and target replica path must be different, terminating.")
-        logging.exception("Identical source and replica path: " + replica_path + " -> terminating")
-        time.sleep(constants.TERMINATION_DELAY)
-        sys.exit()
     if not os.path.exists(source_path):
         # path error
         print("Path is no longer valid.")
         logging.exception("Folder " + source_path + " was moved, renamed or deleted, terminating")
         time.sleep(constants.TERMINATION_DELAY)
         sys.exit()
-    else:
-        # all ok
-        differ = Differ()
-        if len(os.listdir(replica_path)) > 0:
-            print("Selected replica folder is not empty, all files will be deleted. Do you wish to continue? (Y/N)")
-            logging.info("Selected replicating folder is not empty")
-            if input() == "Y":
-                print("Deleting all files.")
-                logging.info("Deleting all files in " + replica_path + ".")
-                shutil.rmtree(replica_path)
-                os.mkdir(replica_path)
-            else:
-                print("Aborting synchronization.")
-                logging.info("Run aborted.")
-                time.sleep(constants.TERMINATION_DELAY)
-                sys.exit()
-        with open(log_path, 'w', encoding="UTF-8") as f:
-            f.write(
-                "This file contains logs for changes at: " + source_path + "\n\nSynchronization started at: " + str(
-                    datetime.now()) + "\n")
-        print("Synchronization started.")
-        logging.info("Synchro started with src: " + source_path + " and rep: " + replica_path)
-        while True:
-            for src_p, src_f in absoluteFilePaths(source_path):
-                if constants.LOG_FILE in src_f:
-                    logging.error(
-                        "Source folder can't contain file named '" + constants.LOG_FILE + "'. Do you wish to remove it? (Y/N)")
-                    if input() == "Y":
-                        os.remove(src_p)
-                    else:
-                        print("Terminating.")
-                        logging.info("Terminating, logging file name collision.")
-                        time.sleep(constants.TERMINATION_DELAY)
-                        sys.exit()
-            time.sleep(constants.SYNC_PERIOD)
-            print("Updating")
-            logging.info("Updating repository at: " + replica_path)
-            syncFolders(differ, log_path)
+    if source_path in replica_path or replica_path in source_path:
+        print("Selected folder can not be subdirectories.")
+        logging.exception(
+            "Selected folders are subdirectories: " + replica_path + " : " + source_path + " -> terminating")
+        time.sleep(constants.TERMINATION_DELAY)
+        sys.exit()
+    # all ok
+    differ = Differ()
+    if len(os.listdir(replica_path)) > 0:
+        print("Selected replica folder is not empty, all files will be deleted. Do you wish to continue? (Y/N)")
+        logging.info("Selected replicating folder is not empty")
+        if input() == "Y":
+            print("Deleting all files.")
+            logging.info("Deleting all files in " + replica_path + ".")
+            shutil.rmtree(replica_path)
+            os.mkdir(replica_path)
+        else:
+            print("Aborting synchronization.")
+            logging.info("Run aborted.")
+            time.sleep(constants.TERMINATION_DELAY)
+            sys.exit()
+    with open(log_path, 'w') as f:
+        f.write(
+            "This file contains logs for changes at: " + source_path + "\n\nSynchronization started at: " + str(
+                datetime.now()) + "\n")
+    print("Synchronization started.")
+    logging.info("Synchro started with src: " + source_path + " and rep: " + replica_path)
+    while True:
+        for src_p, src_f in absoluteFilePaths(source_path):
+            if constants.LOG_FILE in src_f:
+                logging.warning(
+                    "Source folder can't contain file named '" + constants.LOG_FILE + "'. Do you wish to remove it? (Y/N)")
+                if input() == "Y":
+                    os.remove(src_p)
+                else:
+                    print("Terminating.")
+                    logging.info("Terminating, logging file name collision.")
+                    time.sleep(constants.TERMINATION_DELAY)
+                    sys.exit()
+        time.sleep(constants.SYNC_PERIOD)
+        print("Updating")
+        logging.info("Updating repository at: " + replica_path)
+        syncFolders(differ, log_path)
